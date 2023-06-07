1babeb67ae33f99a335d3ce37d23b7ed88e1be23
gorzelvk <gorzelvk@gmail.com>
2023-06-07 15:21:53 +0200

    code cleanup

--- tic-tac-toe-game/src/App.js
+++ tic-tac-toe-game/src/App.js
@@ -58,21 +58,19 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
@@ -107,7 +105,7 @@ function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
-    [5, 7, 8],
+    [6, 7, 8],
     [0, 3, 6],
     [1, 4, 7],
     [2, 5, 8],
